c455e1d4c6969a7f9dd15ebe40eb2224e732d494
Nada <nada@ivas.mobi>
2016-06-14 11:42:43 +0200

    Concatenated Javascript Files

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,21 @@
-var gulp  = require('gulp');
-var gutil = require('gulp-util');
+var gulp  = require('gulp'),
+    gutil = require('gulp-util'),
+    coffee = require('gulp-coffee')
+    concat = require('gulp-concat');
 
-gulp.task('log', function(){
-    gutil.log('Workflows are awesome');
+var coffeeSources = ['components/coffee/tagline.coffee'];
+var jsFiles = 'components/scripts/*.js';
+
+gulp.task('coffee', function() {
+    gulp.src(coffeeSources)
+        .pipe(coffee({ bare: true })
+            .on('error', gutil.log))
+        .pipe(gulp.dest('components/scripts'))
 });
+
+gulp.task('concat', function(){
+    gulp.src(jsFiles)
+        .pipe(concat('script.js'))
+        .pipe(gulp.dest('builds/development/js'))
+});
+
