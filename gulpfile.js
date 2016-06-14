f05de5ca65d6069a5e2caab3d66d103da54dd6d0
Nada <nada@ivas.mobi>
2016-06-14 14:19:29 +0200

    Added watch task

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,12 @@ var gulp  = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
     browserify = require('gulp-browserify'),
+    compass   = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var coffeeSources = ['components/coffee/tagline.coffee'];
 var jsFiles = 'components/scripts/*.js';
+var sassSources = ['components/sass/style.scss'];
 
 gulp.task('coffee', function() {
     gulp.src(coffeeSources)
@@ -19,5 +21,21 @@ gulp.task('js', function(){
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function(){
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'components/sass',
+            image: 'builds/development/images',
+            style: 'expanded'
+        }))
+        .pipe(gulp.dest('builds/development/css'))
+});
+
+gulp.task('watch', function(){
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsFiles, ['js']);
+    gulp.watch('components/sass/*.scss', ['compass']);
 });
 
